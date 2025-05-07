34066e6a88db914b3a42c0a3fac95aad4047b459
samantaavijit <samantaavijit.2000@gmail.com>
2025-05-07 15:55:41 +0530

    changes

--- src/components/Layouts/AfterLoginNavbar.tsx
+++ src/components/Layouts/AfterLoginNavbar.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+// import { Notification } from "./notification";
+import { useSidebarContext } from "./sidebar/sidebar-context";
+import { UserInfo } from "./header/user-info";
+import { MenuIcon } from "./header/icons";
 
 export default function AfterLoginNavbar() {
-  return <div>AfterLoginNavbar</div>;
+  const { toggleSidebar, isMobile } = useSidebarContext();
+
+  return (
+    <header className="sticky top-0 flex items-center justify-between border-b border-stroke bg-white px-4 py-5 shadow-1 dark:border-stroke-dark dark:bg-gray-dark md:px-5 2xl:px-10">
+      <button
+        onClick={toggleSidebar}
+        className="rounded-lg border px-1.5 py-1 dark:border-stroke-dark dark:bg-[#020D1A] hover:dark:bg-[#FFFFFF1A] lg:hidden"
+      >
+        <MenuIcon />
+        <span className="sr-only">Toggle Sidebar</span>
+      </button>
+
+      {isMobile && (
+        <Link href={"/"} className="ml-2 max-[430px]:hidden min-[375px]:ml-4">
+          <Image
+            src={"/images/logo/logo-icon.svg"}
+            width={32}
+            height={32}
+            alt=""
+            role="presentation"
+          />
+        </Link>
+      )}
+
+      <div className="max-xl:hidden">
+        <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
+          Dashboard
+        </h1>
+        <p className="font-medium">Next.js Admin Dashboard Solution</p>
+      </div>
+
+      <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
+        {/* <Notification /> */}
+
+        <div className="shrink-0">
+          <UserInfo />
+        </div>
+      </div>
+    </header>
+  );
 }
