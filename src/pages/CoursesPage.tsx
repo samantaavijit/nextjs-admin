5ad66fd76caa017926e5d38c57556e3ef2d25170
samantaavijit <samantaavijit.2000@gmail.com>
2025-05-08 20:09:06 +0530

    complete

---
+++ src/pages/CoursesPage.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import CourseCard from "@/components/CourseCard";
+import { COURSES } from "@/data/Courses";
+
+export default function CoursesPage() {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-between overflow-hidden">
+      <div className="from-dark-200/50 to-dark-300/50 w-full bg-gradient-to-b pb-16 backdrop-blur-lg sm:py-32">
+        <section className="relative px-6 py-15 text-center md:px-12">
+          <div className="mx-auto max-w-5xl">
+            <h1
+              data-aos="zoom-in"
+              className="text-3xl font-bold text-white md:text-5xl"
+            >
+              Explore Our Expert-Led Courses
+            </h1>
+            <p
+              className="mt-4 text-lg text-gray-300"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
+              Discover our wide range of courses taught by industry experts
+            </p>
+          </div>
+        </section>
+
+        <section className="px-6 py-12 md:px-12">
+          <div className="grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3">
+            {COURSES.map((course, key) => (
+              <div
+                data-aos="fade-right"
+                data-aos-anchor-placement="top-center"
+                data-aos-delay={`${key * 100}`}
+                key={key}
+              >
+                <CourseCard course={course} />
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
